0938634bb9c6e9035dfcb7db8582be2a08de70f6
javierguardiola <guardiola.bajav19@zaragoza.salesianos.edu>
2022-05-25 18:19:58 +0200

    Modificacion definitiva corazon y curaciones

--- Sprits/BarraSalud.gd
+++ Sprits/BarraSalud.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 export (NodePath) var player_node
-onready var player = get_node(player_node)
+onready var player = get_node(player_node) # ahora podremos acceder a todas la variables del player
 
 func _process(_delta):
 	$TextureProgress.value = player.health
